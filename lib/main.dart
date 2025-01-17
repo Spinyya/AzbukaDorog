a7310e6e45cbd5a95fb68a8b8199a4c07b9c2355
Spinyya <1999popovroman@gmail.com>
2025-01-17 20:57:51 +0300

    fix1.0

--- lib/main.dart
+++ lib/main.dart
@@ -16,14 +16,13 @@ class MyApp extends StatelessWidget {
       title: 'Азбука Дорог',
       initialRoute: '/',
       routes: {
-    '/': (context) => HomePage(), // Главная страница
-    '/second': (context) => Lessonpage(), // Вторая страница
-    },
+        '/': (context) => HomePage(), // Главная страница
+        '/second': (context) => Lessonpage(), // Вторая страница
+      },
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      home: const HomePage (),
     );
   }
 
