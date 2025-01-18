053e64372c8a19eb6c789034c945c58968dbbb90
Spinyya <1999popovroman@gmail.com>
2025-01-18 11:38:58 +0300

    bb

--- lib/pages/lessonpage/lessonpage.dart
+++ lib/pages/lessonpage/lessonpage.dart
@@ -1,23 +1,25 @@
+import 'package:azbukadorog/design/images.dart';
 import 'package:flutter/material.dart';
 import 'package:azbukadorog/pages/homepage/button.dart';
 import '../../design/colors.dart';
+import 'package:azbukadorog/main.dart';
 
 class Lessonpage extends StatelessWidget {
   const Lessonpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: <Widget>[_text()],);
+    return Stack(children: <Widget>[_text(context)],);
 
   }
 
-  Widget _text() {
+  Widget _text(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Азбука Дорог', style: TextStyle(
           color: textColor,
         ),),
-
+        leading: IconButton(icon: exitIm, onPressed: () {Navigator.pushNamed(context, '/');},),
         centerTitle: true,
         backgroundColor: primaryColor,
 
