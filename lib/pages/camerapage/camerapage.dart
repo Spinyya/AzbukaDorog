7cc90a09404c104cdc7e26e5414c1b476600427d
Spinyya <1999popovroman@gmail.com>
2025-02-06 12:58:42 +0300

    main

--- lib/pages/camerapage/camerapage.dart
+++ lib/pages/camerapage/camerapage.dart
@@ -2,13 +2,17 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pytorch_lite/pytorch_lite.dart';
 
 class TakePictureScreen extends StatefulWidget {
   const TakePictureScreen({
     super.key,
     required this.camera,
   });
+
   final CameraDescription camera;
+
   @override
   TakePictureScreenState createState() => TakePictureScreenState();
 }
@@ -20,98 +24,110 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   @override
   void initState() {
     super.initState();
-    // To display the current output from the Camera,
-    // create a CameraController.
+    // To display the current output from the Camera, create a CameraController.
     _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
+      // Get the specified camera.
       widget.camera,
       // Define the resolution to use.
       ResolutionPreset.medium,
-      
     );
 
-    // Next, initialize the controller. This returns a Future.
+    // Initialise the controller.
     _initializeControllerFuture = _controller.initialize();
-    
   }
 
   @override
   void dispose() {
     // Dispose of the controller when the widget is disposed.
-    _controller.initialize();
+    _controller.dispose();
     super.dispose();
   }
 
+
   @override
   Widget build(BuildContext context) {
+    return Stack(children: <Widget>[camera_prewiew(context), Align(alignment: Alignment.bottomCenter, child: button(context),)]);
+  }
+
+  Widget camera_prewiew(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Сделайте фото')),
-      
+      appBar: AppBar(title: const Text('Take a picture')),
       // You must wait until the controller is initialized before displaying the
       // camera preview. Use a FutureBuilder to display a loading spinner until the
       // controller has finished initializing.
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
-          
           if (snapshot.connectionState == ConnectionState.done) {
             // If the Future is complete, display the preview.
-            return SafeArea(child: Padding(padding: EdgeInsets.only(left: 20, right: 20), child: RotationTransition(turns: AlwaysStoppedAnimation(90/360), child: CameraPreview(_controller))));
+            return CameraPreview(_controller);
           } else {
             // Otherwise, display a loading indicator.
             return const Center(child: CircularProgressIndicator());
           }
         },
       ),
-      floatingActionButton: FloatingActionButton(
-        // Provide an onPressed callback.
-        onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
-          try {
-            // Ensure that the camera is initialized.
-            await _initializeControllerFuture;
 
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
-            final image = await _controller.takePicture();
+    );
+  }
 
-            if (!context.mounted) return;
+  Widget button(BuildContext context) {
+    return SafeArea(child: Padding(padding: EdgeInsets.only(bottom: 100), child:IconButton(
+      onPressed: () async {
+        // Take the Picture in a try/catch block. If anything goes wrong,
+        // catch the error.
+        try {
+          // Ensure that the camera is initialized.
+          await _initializeControllerFuture;
+          // Attempt to take a picture and get the file `image`
+          // where it was saved.
+          final image = await _controller.takePicture();
 
-            // If the picture was taken, display it on a new screen.
-            await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
-                  imagePath: image.path,
-                ),
-              ),
-            );
-          } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-          }
-        },
-        child: const Icon(Icons.camera_alt),
-      ),
+          if (!mounted) return;
+          loadModel(image);
+          await Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) =>
+                DisplayPictureScreen(imagePath: image.path,),),);
+        } catch (e) {
+          print(e);
+        }
+      },
+      icon: Icon(Icons.camera_alt),
+      iconSize: 50,
+
+    )
+    )
     );
   }
 }
-
 // A widget that displays the picture taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
+class DisplayPictureScreen extends StatelessWidget{
   final String imagePath;
 
   const DisplayPictureScreen({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
+    var result = loadModel(imagePath);
+    print(result);
     return Scaffold(
       appBar: AppBar(title: const Text('Display the Picture')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
-      body: Image.file(File(imagePath)),
+
+      body: Image.file(File(imagePath))
+
+
     );
+
   }
+}
+
+Future loadModel(image) async {
+  String pathImageModel = "assets/best.pt";
+  ClassificationModel imageModel = await PytorchLite.loadClassificationModel(
+      pathImageModel, 224, 224, 41,
+      labelPath: "assets/classesname.txt");
+  String imagePrediction = await imageModel.getImagePrediction(await File(image.path).readAsBytes());
+  return imagePrediction;
 }
---
+++ lib/pages/model/model.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:pytorch_lite/pytorch_lite.dart';
+Future<void> load_model() async {
+  ClassificationModel classificationModel= await PytorchLite.loadClassificationModel(
+      "assets/best.pt", 224, 224, 41, labelPath: "assets/classesname.txt");
+  String imagePrediction = await classificationModel.getImagePrediction(await File('assets/test7.jpg').readAsBytes());
+}
