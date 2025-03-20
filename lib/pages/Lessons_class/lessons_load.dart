4000085f4c89cdf81e2a425e822707d96f379103
rtyu240 <1999popovroman@gmail.com>
2025-03-20 17:52:13 +0300

    main

--- lib/pages/Lessons_class/lessons_load.dart
+++ lib/pages/Lessons_class/lessons_load.dart
@@ -1,17 +1,16 @@
-import "package:sqflite/sqflite.dart";
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart';
+import 'dart:convert';
+class lessons extends StatelessWidget {
+  final int number;
+  const lessons({super.key, required this.number});
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
 
-class DBProvider {
-  static final DBProvider db = DBProvider();
-  Database? _database;
-
-  //Future<Database> get database async{
-    ///if (_database != null) return _database!;
-    //_database = await initDB();
+    );
   }
-  initDB() async {
 
-  }
-//}
+}
+
